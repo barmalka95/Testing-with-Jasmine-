f9bb97a90c3163b6138187dab6e51e170e1bb7df
Bar Malka <123038326+barmalka95@users.noreply.github.com>
2023-02-02 16:55:11 -0800

    Add files via upload

---
+++ jasmine-testing-exercises/tip-pool/servers.test.js
@@ -0,0 +1,22 @@
+describe("Servers test (with setup and tear-down)", function() {
+  beforeEach(function () {
+    // initialization logic
+    serverNameInput.value = 'Alice';
+  });
+
+  it('should add a new server to allServers on submitServerInfo()', function () {
+    submitServerInfo();
+
+    expect(Object.keys(allServers).length).toEqual(1);
+    expect(allServers['server' + serverId].serverName).toEqual('Alice');
+  });
+
+  it('should not add a new server on submitServerInfo() if the input is empty', function () {
+    server
+  })
+
+  afterEach(function() {
+    // teardown logic
+    serverTbody.innerHTML = '';
+  });
+});
